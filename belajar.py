8b9839f97c11c75e78011aa792db895c6964faa9
ceamberly <ceamberly@gmail.com>
2026-03-20 10:12:08 +0700

    Create belajar.py
    tugas

---
+++ belajar.py
@@ -0,0 +1,30 @@
+# Latihan 1.1 - Tipe Data dan Variabel
+
+# -----------------------------
+# Data angka (integer / bilangan bulat)
+umur = 19
+
+# Data angka desimal (float)
+tinggi_badan = 172.3
+
+# Data string (kata/kalimat)
+nama = "Kendy Akbar"
+
+# Data boolean (True / False)
+is_mahasiswa = True
+
+# -----------------------------
+# Menampilkan nilai variabel
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi Badan:", tinggi_badan)
+print("Mahasiswa:", is_mahasiswa)
+
+print()  
+
+# -----------------------------
+# Menampilkan tipe data masing-masing variabel
+print("Tipe data nama:", type(nama))
+print("Tipe data umur:", type(umur))
+print("Tipe data tinggi_badan:", type(tinggi_badan))
+print("Tipe data is_mahasiswa:", type(is_mahasiswa))
